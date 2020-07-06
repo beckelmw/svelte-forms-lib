a163ff3c7a15814ce334fac54e82d49da31a55a3
Bill Beckelman <bill@beckelman.net>
2020-07-06 12:39:29 -0700

    Added textarea

---
+++ lib/components/TextArea.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { getContext } from "svelte";
+  import { key } from "./key";
+
+  export let name;
+
+  const { form, handleChange } = getContext(key);
+</script>
+
+<textarea
+  {name}
+  value={$form[name]}
+  on:change={handleChange}
+  on:blur={handleChange}
+  {...$$props} />
